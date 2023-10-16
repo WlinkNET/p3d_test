fca3c6fe8f3705ad202a6a2244ee0ee1911c8cb2
WlinkNET <nemanjadjukic@yahoo.com>
2023-10-16 20:47:30 +0000

    select_top_all_4

--- src/polyline.rs
+++ src/polyline.rs
@@ -279,38 +279,24 @@ impl GenPolyLines {
     pub (crate) fn select_top_all_4(
         cntrs: &Vec<Vec<Vec2>>, depth: usize, grid_size: usize, rect: Rect,
     ) -> Vec<Vec<(f64, Vec<u8>)>> {
-
-        let mut top_heap: Vec<Vec<(f64, Vec<u8>)>> = Vec::with_capacity(grid_size as usize);
-
-        for cntr in cntrs.iter() {
+        let top_heap: Arc<Mutex<Vec<Vec<(f64, Vec<u8>)>>>> = Arc::new(Mutex::new(Vec::with_capacity(grid_size as usize)));
+    
+        cntrs.par_iter().for_each(|cntr| {
             let mut top_in_cntr: Vec<(f64, PolyLine)> = Vec::with_capacity(depth);
             let cn = Cntr::new(Some(cntr.to_vec()), grid_size as i16, &rect);
             let zone = cn.line_zone();
             let mut gen_lines = GenPolyLines::new(zone, grid_size as i16);
             let start_point = Point2 { x: 0, y: 0 };
-
             gen_lines.line_buf.nodes.push(start_point);
-
+    
             let cntr_size = cn.points.len();
-            let calc_sco = |pl: &PolyLine|
-                GenPolyLines::sco2(
-                    &cn, &pl.line2points(cntr_size, &rect),
-                );
-
+            let calc_sco = |pl: &PolyLine| GenPolyLines::sco2(&cn, &pl.line2points(cntr_size, &rect));
+    
             let mut ff = |pl: &PolyLine| {
                 let d = calc_sco(pl);
-
-                if let Some(_) = top_in_cntr.iter().find(|a| a.0 == d) {
-                    return
-                }
-                else {
+                if top_in_cntr.iter().find(|a| a.0 == d).is_none() {
                     if top_in_cntr.len() == depth {
-                        let m = top_in_cntr.iter()
-                            .enumerate()
-                            .max_by(|(_, a), (_, b)|
-                                a.0.partial_cmp(&b.0).unwrap_or(core::cmp::Ordering::Equal)
-                            );
-
+                        let m = top_in_cntr.iter().enumerate().max_by(|(_, a), (_, b)| a.0.partial_cmp(&b.0).unwrap_or(core::cmp::Ordering::Equal));
                         if let Some((i, r)) = m {
                             if r.0 > d {
                                 top_in_cntr[i] = (d, pl.clone());
@@ -322,10 +308,14 @@ impl GenPolyLines {
                 }
             };
             gen_lines.complete_line(&mut ff);
-            top_heap.push(top_in_cntr.into_iter().map(|a| (a.0, a.1.calc_hash().to_vec())).collect());
-        }
-        top_heap
+    
+            let mut top_heap_lock = top_heap.lock();
+            top_heap_lock.push(top_in_cntr.into_iter().map(|a| (a.0, a.1.calc_hash().to_vec())).collect());
+        });
+    
+        Arc::try_unwrap(top_heap).unwrap().lock().to_vec()
     }
+    
 
     fn complete_line<F>(&mut self, f: &mut F)
         where
